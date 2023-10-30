6fa898ff1d9e6415e5544e435ca85dc2e3a4b01b
Seongsu Park <56729421+seongs1024@users.noreply.github.com>
2023-10-30 19:00:21 +0900

    make an example

---
+++ examples/finder.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+use economic_indicator_finder::{Finder, Indicator, Speech};
+use std::fs::read_to_string;
+
+fn main() -> Result<()> {
+    let speechs = Speech::from_json_file("speech/speech.json")?;
+    let finder = Finder::new(Indicator::from_json_file("asset/indicators.json")?)?;
+
+    for speech in speechs {
+        let speaker = &speech.speaker;
+        let speech = read_to_string(speech.file)?;
+        let matches: Vec<_> = finder.matches(speech);
+        println!("From the speech of {}: {:#?}", speaker, matches);
+    }
+    Ok(())
+}
